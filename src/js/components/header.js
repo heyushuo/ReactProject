f483e0fa879505430b17a53a5be91fe4bc39ec90
heyushuo <1007561607@qq.com>
2018-02-01 10:39:40 +0800

    class

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,9 +3,19 @@ import ReactDom from 'react-dom';
 
 export default class Header extends React.Component{
 	render(){
+		//需要放到render函数里边
+		//行内样式
+		const style={
+			header: {
+				color: "red",
+				backgroundColor: "yellow",
+				paddingTop: "15px",
+				paddingBottom: "20px"
+			}
+		}
 		return (
-			<header>
-				<h1>我是头部 我的老大是何玉硕</h1>
+			<header className="smallFont" style={style.header} >
+				<h1>我是头部 我的老大是何玉硕 我是头部 我是头部</h1>
 			</header>
 		)
 	}
